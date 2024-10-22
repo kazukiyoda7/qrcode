d0b3c6706749d528530a2ddeef2352be7b5d0095
kazukiyoda7 <yodakazu@icloud.com>
2024-10-23 01:19:37 +0900

    base

--- generate.py
+++ generate.py
@@ -1,17 +1,19 @@
 import qrcode
 
 qr = qrcode.QRCode(
-    version=3,
+    version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=10,
-    border=2,
+    border=1,
+    mask_pattern=0
 )
 
 for i in range(10):
-    filename = str(i)+'.png'
-    qr.add_data(filename)
+    data = str(i)
+    qr.add_data(data)
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white").convert('L')
-    img.save('data/' + filename)
+    img.save('data/' + data + '.png')
 
+    qr.clear()
