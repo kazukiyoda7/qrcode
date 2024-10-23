3fbf52b8b9e846fa163e850c6231f8b687b62d5c
yodakazu <yodystudying@gmail.com>
2024-10-23 15:56:30 +0900

    a

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import qrcode
+import numpy as np
 
 qr = qrcode.QRCode(
     version=1,
@@ -11,7 +12,14 @@ qr = qrcode.QRCode(
 for i in range(10):
     data = str(i)
     qr.add_data(data)
+    # print(qr.data_list)
     qr.make(fit=True)
+    # print(qr.modules)
+    
+    # バイナリデータ化
+    # data_binary = np.array(qr.modules).astype(int).flatten()
+    # print(data_binary)
+    # print(data_binary.shape)
 
     img = qr.make_image(fill_color="black", back_color="white").convert('L')
     img.save('data/' + data + '.png')
